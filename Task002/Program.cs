003a019cbe1ec6a2ae5abc70065efe1b135c22bf
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-04 16:24:16 +0300

    Task 002 added

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine( ));
+
+    
+int max = a;
+
+if (a>max) max = a;    
+if (b>max) max = b;
+if (c>max) max = c;
+    
+System.Console.WriteLine($"{max} = max");
+  
+
+
