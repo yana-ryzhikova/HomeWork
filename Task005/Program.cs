353a5c56bca115ea6d0ea3ce4b93282275b3dc87
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-07 01:44:42 +0300

    Added Task 005

---
+++ Task005/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+//вторую цифру этого числа. Не использовать строки для расчета.
+//
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число: ");
+if (number > 99 & number < 1000)
+{
+    int result = number / 10 % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Число не является трехзначным");
+}
