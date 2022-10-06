0cafb46dbe046bd1226eb52826d2f50362ce23c6
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-07 01:45:16 +0300

    Added Task 007

---
+++ Task007/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+//
+//645 -> 5
+//78 -> третьей цифры нет
+//326792 -> 6
+
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = Prompt("Введите число: ");
+if (number > 99 && number < 1000)
+{
+    int result = number % 10;
+    System.Console.WriteLine(result);
+}
+else if (number >= 1000)
+{
+while (number > 999)
+{
+    number /= 10;
+}
+    int result = number % 10;
+    System.Console.WriteLine(result);
+}
+else
+{
+    System.Console.WriteLine("Число не содержит третью цифру.");
+}
